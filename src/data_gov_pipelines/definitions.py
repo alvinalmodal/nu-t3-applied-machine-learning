538b668d0b1d698c1d8d94269cf530d4ccb30f8e
Alvin B. Almodal <almodalalvin@gmail.com>
2025-05-08 09:15:39 +0800

    Border crossing silver and gold layers (#12)
    * added silver layer for border crossing assets
    
    * added gold layer tables and databases

--- src/data_gov_pipelines/definitions.py
+++ src/data_gov_pipelines/definitions.py
@@ -1,11 +1,11 @@
 import dagster as dg
-from data_gov_pipelines.assets.border_crossing import border_crossing_csv
+from data_gov_pipelines.assets.border_crossing import bronze_borders_crossing, silver_borders_crossing, \
+    gold_borders_crossing_total_migration_by_port_code
 from data_gov_pipelines.job.customer_order_workflow import customer_order_workflow
 from data_gov_pipelines.schedule.customer_order_workflow_schedule import customer_order_workflow_schedule
 
 defs = dg.Definitions(
-    assets=[border_crossing_csv],
-    jobs=[customer_order_workflow],
+    assets=[bronze_borders_crossing, silver_borders_crossing, gold_borders_crossing_total_migration_by_port_code],
     schedules=[customer_order_workflow_schedule],
     resources={},
 )
